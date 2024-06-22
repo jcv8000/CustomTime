2b07068aa53d74b9e6d9bde1205ed20a2577b90d
Josh Vickery <jcv8000@gmail.com>
2024-06-22 00:57:34 -0500

    api version

--- src/main/java/dev/jvickery/customtime/CustomTime.java
+++ src/main/java/dev/jvickery/customtime/CustomTime.java
@@ -1,13 +1,16 @@
 package dev.jvickery.customtime;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.annotation.dependency.SoftDependency;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
 import org.bukkit.plugin.java.annotation.plugin.Description;
 import org.bukkit.plugin.java.annotation.plugin.LogPrefix;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 import org.bukkit.plugin.java.annotation.plugin.Website;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion.Target;
 import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
 @Plugin(name="CustomTime", version="3.0")
+@ApiVersion(Target.v1_17)
 @Description("Plugin to control the speed of the day/night cycle")
 @Author("jcv8000")
 @Website("https://github.com/jcv8000/CustomTime")
